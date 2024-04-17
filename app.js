993f0a8ad2b479f448d72a428eeae0fa524ee056
ThiagoFirmeza <102990850+ThiagoFirmeza@users.noreply.github.com>
2024-04-17 17:19:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function salvarDados () {
+    const nomeTutor = document.getElementById("nomeTutor");
+    const nomeCachorro = document.getElementById("nomeCachorro");
+    const racaCachorro = document.getElementById("racaCachorro");
+    alert(nomeTutor + "Cadastrado com sucesso")
+    alert(nomeCachorro + "Cadastrado com sucesso")
+    alert(racaCachorro + "Cadastrado com sucesso")
+}
